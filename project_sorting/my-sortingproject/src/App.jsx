31d0066974fbe53e22b9963ba7d1899e8c8f82a7
jyothika1803 <jyothikarajesh1809@gmail.com>
2025-02-23 16:31:05 +0530

    project_without_correct_alignment

--- project_sorting/my-sortingproject/src/App.jsx
+++ project_sorting/my-sortingproject/src/App.jsx
@@ -1,54 +1,18 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Algo from "./pages/Algo";
+
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="title">Sorting Algorithms</h1>
-      <p className="description">
-        Sorting algorithms rearrange elements based on a comparison operation. Here are some common ones:
-      </p>
-
-      <div className="grid-container">
-        {/* First row - 3 Sorting Algorithms */}
-        <div className="row">
-          <div className="sorting-box">
-            <h4>Bubble Sort</h4>
-            <img src="/images/bubblesort.png" alt="Bubble Sort" />
-            <button>Learn More</button>
-          </div>
-
-          <div className="sorting-box">
-            <h4>Selection Sort</h4>
-            <img src="/images/selectionsort.png" alt="Selection Sort" />
-            <button>Learn More</button>
-          </div>
-
-          <div className="sorting-box">
-            <h4>Insertion Sort</h4>
-            <img src="/images/insertionsort.png" alt="Insertion Sort" />
-            <button>Learn More</button>
-          </div>
-        </div>
-
-        {/* Second row - 2 Sorting Algorithms centered */}
-        <div className="row center">
-          <div className="sorting-box">
-            <h4>Merge Sort</h4>
-            <img src="/images/mergesort.png" alt="Merge Sort" />
-            <button>Learn More</button>
-          </div>
-
-          <div className="sorting-box">
-            <h4>Quick Sort</h4>
-            <img src="/images/quicksort.png" alt="Quick Sort" />
-            <button>Learn More</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/visualize/:sortType" element={<Algo />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
